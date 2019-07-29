ae7e6856ce5ada456a7bef24c5a74227789ea705
tulshi das <bsse0811@iit.du.ac.bd>
2019-07-29 18:16:42 +0600

    bold maker

--- stack/res/js/answer.js
+++ stack/res/js/answer.js
@@ -1,17 +1,87 @@
 "use strict";
 class Answer {
+    constructor() {
+        this.answerEditor = document.getElementById('answer_editor');
+    }
     init() {
         this.setInputListenerInAnswerEditor();
+        this.addBoldListener();
     }
     setInputListenerInAnswerEditor() {
         document.getElementById('answer_editor').oninput = () => {
-            let text = document.getElementById('answer_editor').value;
+            let text = this.answerEditor.value;
             this.showFormatedAnswer(text);
         };
     }
+    addBoldListener() {
+        document.getElementById('boldBtn').addEventListener('click', () => {
+            let value = this.answerEditor.value;
+            let startIndex = this.answerEditor.selectionStart;
+            let endIndex = this.answerEditor.selectionEnd;
+            if (this.isBold(startIndex, endIndex)) {
+                this.removeBold(startIndex, endIndex);
+                return;
+            }
+            this.addBold(startIndex, endIndex);
+            //    let first_part = value.substring(0, startIndex);
+            //    let second_part = value.substring(endIndex,value.length);
+            //    this.answerEditor.value = first_part +"**write here**" + second_part;
+            //    this.selectText(startIndex+2, startIndex+12);
+        });
+    }
+    addBold(startIndex, endIndex) {
+        let value = this.answerEditor.value;
+        let first_part = value.substring(0, startIndex);
+        let second_part = value.substring(endIndex, value.length);
+        let middle_part = value.substring(startIndex, endIndex);
+        if (middle_part == "")
+            middle_part = "write here";
+        this.answerEditor.value = first_part + "**" + middle_part + "**" + second_part;
+        if (middle_part == "")
+            this.selectText(startIndex + 2, startIndex + 12);
+    }
+    removeBold(startIndex, endIndex) {
+        let value = this.answerEditor.value;
+        let first_part = value.substring(0, startIndex - 2);
+        let second_part = value.substring(endIndex + 2, value.length);
+        let middle_part = value.substring(startIndex, endIndex);
+        this.answerEditor.value = first_part + middle_part + second_part;
+    }
+    isBold(startIndex, endIndex) {
+        let value = this.answerEditor.value;
+        let prefix = value.substring(startIndex - 2, startIndex);
+        let suffix = value.substring(endIndex, endIndex + 2);
+        if (prefix == "**" && suffix == "**")
+            return true;
+        return false;
+    }
+    addItalicListener() {
+    }
+    addUnderLineListener() {
+    }
+    addLinkListener() {
+    }
+    addNewLineListener() {
+    }
+    addOrderListListener() {
+    }
+    addUnorderListListener() {
+    }
+    addImageListener() {
+    }
+    addHeadingener() {
+    }
     showFormatedAnswer(text) {
         let elem = document.getElementById('answer_display');
-        elem.innerHTML = text;
+        elem.innerHTML = this.getHtml(text);
+    }
+    getHtml(text) {
+        return "";
+    }
+    selectText(startPos, endPos) {
+        const input = document.getElementById('answer_editor');
+        input.focus();
+        input.setSelectionRange(startPos, endPos);
     }
 }
 new Answer().init();
--- stack/ts/answer.ts
+++ stack/ts/answer.ts
@@ -1,59 +1,104 @@
 
-class Answer{
+class Answer {
+    private answerEditor = <HTMLInputElement>document.getElementById('answer_editor');
 
-    public init(){
+    public init() {
         this.setInputListenerInAnswerEditor();
+        this.addBoldListener();
+
     }
 
-    private setInputListenerInAnswerEditor(){
-        document.getElementById('answer_editor').oninput = ()=>{
-            let text = (<HTMLInputElement>document.getElementById('answer_editor')).value;
+
+    private setInputListenerInAnswerEditor() {
+        document.getElementById('answer_editor').oninput = () => {
+            let text = this.answerEditor.value;
             this.showFormatedAnswer(text);
         }
     }
 
-    private addBoldListener(){
-        
+    private addBoldListener() {
+        document.getElementById('boldBtn').addEventListener('click', ()=>{
+           let value = this.answerEditor.value;
+           let startIndex = this.answerEditor.selectionStart;
+           let endIndex = this.answerEditor.selectionEnd;
+           if(this.isBold(startIndex,endIndex)){
+                this.removeBold(startIndex,endIndex);
+                return;
+           }
+           this.addBold(startIndex,endIndex);
+        });
     }
 
-    private addItalicListener(){
+    private addBold(startIndex:number, endIndex:number){
+        let value = this.answerEditor.value;
+        let first_part = value.substring(0,startIndex);
+        let second_part = value.substring(endIndex,value.length);
+        let middle_part = value.substring(startIndex,endIndex);
+        if(middle_part == "") middle_part = "write here";
+        this.answerEditor.value = first_part +"**"+ middle_part+"**" + second_part;
         
+        if(middle_part == "")this.selectText(startIndex+2, startIndex+12);
     }
 
-    private addUnderLineListener(){
-        
+    private removeBold(startIndex:number,endIndex:number){
+        let value = this.answerEditor.value;
+        let first_part = value.substring(0,startIndex-2);
+        let second_part = value.substring(endIndex+2,value.length);
+        let middle_part = value.substring(startIndex,endIndex);
+        this.answerEditor.value = first_part + middle_part + second_part;
     }
 
-    private addLinkListener(){
-        
+    private isBold(startIndex:number,endIndex:number):boolean{
+        let value = this.answerEditor.value;
+        let prefix = value.substring(startIndex-2,startIndex);
+        let suffix = value.substring(endIndex,endIndex+2);
+        if(prefix == "**" && suffix == "**") return true;
+        return false;
     }
 
-    private addNewLineListener(){
-        
+    private addItalicListener() {
+
     }
 
-    private addOrderListListener(){
-        
-    }
-    private addUnorderListListener(){
-        
+    private addUnderLineListener() {
+
     }
 
-    private addImageListener(){
-        
+    private addLinkListener() {
+
     }
 
-    private addHeadingener(){
-        
+    private addNewLineListener() {
+
     }
 
-    private showFormatedAnswer(text:string){
+    private addOrderListListener() {
+
+    }
+    private addUnorderListListener() {
+
+    }
+
+    private addImageListener() {
+
+    }
+
+    private addHeadingener() {
+
+    }
+
+    private showFormatedAnswer(text: string) {
         let elem = document.getElementById('answer_display');
         elem.innerHTML = this.getHtml(text);
     }
 
-    private getHtml(text:string):string{
-
+    private getHtml(text: string): string {
+        return "";
+    }
+    private selectText(startPos: number, endPos: number) {
+        const input = <HTMLInputElement>document.getElementById('answer_editor');
+        input.focus();
+        input.setSelectionRange(startPos, endPos);
     }
 }
 
