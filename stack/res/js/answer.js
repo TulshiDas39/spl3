1e864a8c64ea18800c1b47c650a8ac67d1e8ce05
tulshi das <bsse0811@iit.du.ac.bd>
2019-07-30 18:27:03 +0600

    input formatting in answer editor

--- stack/res/js/answer.js
+++ stack/res/js/answer.js
@@ -2,68 +2,134 @@
 class Answer {
     constructor() {
         this.answerEditor = document.getElementById('answer_editor');
+        this.boldTags = ['<bold>', '</bold>'];
+        this.italicTags = ['<italic>', '</italic>'];
+        this.underLineTags = ['<underLine>', '</underLine>'];
+        this.linkTags = ['<link url = "">', '</link>'];
+        this.tabTag = '<tab>';
+        this.unorderListTags = ['<unorder-list>', '</unorder-list>'];
+        this.orderListTags = ['<order-list>', '</order-list>'];
+        this.listItemTags = ['<list-item>', '</list-item>'];
     }
     init() {
         this.setInputListenerInAnswerEditor();
         this.addBoldListener();
+        this.addItalicListener();
+        this.addUnderLineListener();
+        this.addLinkListener();
+        this.addTabListener();
+        this.addOrderListListener();
+        this.addUnOrderListListener();
     }
     setInputListenerInAnswerEditor() {
         document.getElementById('answer_editor').oninput = () => {
             let text = this.answerEditor.value;
-            this.showFormatedAnswer(text);
+            let html = this.replaceNewLineByBreakTag(text);
+            this.showFormatedAnswer(html);
         };
+    }
+    // private replaceUnnecessaryLineBreak(text:string):string{
+    //     let res = text.replace(this.orderListTags[0]+"\n\t"+"/g", this.orderListTags[0]);
+    //     //'\n'+this.orderListTags[1]
+    //     res = res.replace('\n'+this.orderListTags[1]+"/g", this.orderListTags[0]);
+    //     return res;
+    // }
+    replaceNewLineByBreakTag(text) {
+        let res = text.replace(/\n/g, "<br>");
+        return res;
     }
     addBoldListener() {
         document.getElementById('boldBtn').addEventListener('click', () => {
-            let value = this.answerEditor.value;
-            let startIndex = this.answerEditor.selectionStart;
-            let endIndex = this.answerEditor.selectionEnd;
-            if (this.isBold(startIndex, endIndex)) {
-                this.removeBold(startIndex, endIndex);
-                return;
-            }
-            this.addBold(startIndex, endIndex);
-            //    let first_part = value.substring(0, startIndex);
-            //    let second_part = value.substring(endIndex,value.length);
-            //    this.answerEditor.value = first_part +"**write here**" + second_part;
-            //    this.selectText(startIndex+2, startIndex+12);
+            this.toogleTag(this.boldTags);
         });
     }
-    addBold(startIndex, endIndex) {
+    addItalicListener() {
+        document.getElementById('italicBtn').addEventListener('click', () => {
+            this.toogleTag(this.italicTags);
+        });
+    }
+    addLinkListener() {
+        document.getElementById('linkBtn').addEventListener('click', () => {
+            this.toogleTag(this.linkTags);
+        });
+    }
+    addUnderLineListener() {
+        document.getElementById('underLineBtn').addEventListener('click', () => {
+            this.toogleTag(this.underLineTags);
+        });
+    }
+    addTabListener() {
+        document.getElementById('tabBtn').addEventListener('click', () => {
+            this.insertTag(this.tabTag);
+        });
+    }
+    addOrderListListener() {
+        document.getElementById('orderListBtn').addEventListener('click', () => {
+            let startIndex = this.answerEditor.selectionStart;
+            this.insertTag(this.orderListTags[0] + "\n\t");
+            this.insertTag(this.listItemTags[0] + "write here" + this.listItemTags[1]);
+            this.insertTag('\n' + this.orderListTags[1]);
+            let selectionStart = startIndex + this.orderListTags[0].length + 2 + this.listItemTags[0].length;
+            this.selectText(selectionStart, selectionStart + "write here".length);
+        });
+    }
+    addUnOrderListListener() {
+        document.getElementById('unorderListBtn').addEventListener('click', () => {
+            let startIndex = this.answerEditor.selectionStart;
+            this.insertTag(this.unorderListTags[0] + "\n\t");
+            this.insertTag(this.listItemTags[0] + "write here" + this.listItemTags[1]);
+            this.insertTag('\n' + this.unorderListTags[1]);
+            let selectionStart = startIndex + this.unorderListTags[0].length + 2 + this.listItemTags[0].length;
+            this.selectText(selectionStart, selectionStart + "write here".length);
+        });
+    }
+    insertTag(tag) {
+        let startIndex = this.answerEditor.selectionStart;
+        let value = this.answerEditor.value;
+        let first_part = value.substring(0, startIndex);
+        let second_part = value.substring(startIndex, value.length);
+        this.answerEditor.value = first_part + tag + second_part;
+    }
+    toogleTag(tags) {
+        let value = this.answerEditor.value;
+        let startIndex = this.answerEditor.selectionStart;
+        let endIndex = this.answerEditor.selectionEnd;
+        if (this.isTagExist(startIndex, endIndex, tags)) {
+            this.removeTag(startIndex, endIndex, tags);
+            return;
+        }
+        this.addTag(startIndex, endIndex, tags);
+    }
+    addTag(startIndex, endIndex, tags) {
         let value = this.answerEditor.value;
         let first_part = value.substring(0, startIndex);
         let second_part = value.substring(endIndex, value.length);
         let middle_part = value.substring(startIndex, endIndex);
-        if (middle_part == "")
+        if (middle_part == "") {
             middle_part = "write here";
-        this.answerEditor.value = first_part + "**" + middle_part + "**" + second_part;
-        if (middle_part == "")
-            this.selectText(startIndex + 2, startIndex + 12);
+        }
+        this.answerEditor.value = first_part + tags[0] + middle_part + tags[1] + second_part;
+        this.selectText(startIndex + tags[0].length, startIndex + middle_part.length + tags[0].length);
     }
-    removeBold(startIndex, endIndex) {
+    removeTag(startIndex, endIndex, tags) {
         let value = this.answerEditor.value;
-        let first_part = value.substring(0, startIndex - 2);
-        let second_part = value.substring(endIndex + 2, value.length);
+        let first_part = value.substring(0, startIndex - tags[0].length);
+        let second_part = value.substring(endIndex + tags[1].length, value.length);
         let middle_part = value.substring(startIndex, endIndex);
+        if (middle_part == "write here")
+            middle_part = "";
         this.answerEditor.value = first_part + middle_part + second_part;
+        this.selectText(startIndex - tags[0].length, startIndex + middle_part.length - tags[0].length);
     }
-    isBold(startIndex, endIndex) {
+    isTagExist(startIndex, endIndex, tags) {
         let value = this.answerEditor.value;
-        let prefix = value.substring(startIndex - 2, startIndex);
-        let suffix = value.substring(endIndex, endIndex + 2);
-        if (prefix == "**" && suffix == "**")
+        let prefix = value.substring(startIndex - tags[0].length, startIndex);
+        let suffix = value.substring(endIndex, endIndex + tags[1].length);
+        if (prefix == tags[0] && suffix == tags[1])
             return true;
         return false;
     }
-    addItalicListener() {
-    }
-    addUnderLineListener() {
-    }
-    addLinkListener() {
-    }
     addNewLineListener() {
-    }
-    addOrderListListener() {
     }
     addUnorderListListener() {
     }
@@ -73,7 +139,8 @@ class Answer {
     }
     showFormatedAnswer(text) {
         let elem = document.getElementById('answer_display');
-        elem.innerHTML = this.getHtml(text);
+        //elem.innerHTML = this.getHtml(text);
+        elem.innerHTML = text;
     }
     getHtml(text) {
         return "";
--- stack/ts/answer.ts
+++ stack/ts/answer.ts
@@ -1,10 +1,26 @@
 
 class Answer {
     private answerEditor = <HTMLInputElement>document.getElementById('answer_editor');
+    private answerInHtml: string;
+    private boldTags = ['<bold>', '</bold>'];
+    private italicTags = ['<italic>', '</italic>'];
+    private underLineTags = ['<underLine>', '</underLine>'];
+    private linkTags = ['<link url = "">','</link>'];
+    private tabTag = '<tab>';
+    private unorderListTags = ['<unorder-list>','</unorder-list>'];
+    private orderListTags = ['<order-list>','</order-list>'];
+    private listItemTags = ['<list-item>','</list-item>'];
+
 
     public init() {
         this.setInputListenerInAnswerEditor();
         this.addBoldListener();
+        this.addItalicListener();
+        this.addUnderLineListener();
+        this.addLinkListener();
+        this.addTabListener();
+        this.addOrderListListener();
+        this.addUnOrderListListener();
 
     }
 
@@ -12,69 +28,135 @@ class Answer {
     private setInputListenerInAnswerEditor() {
         document.getElementById('answer_editor').oninput = () => {
             let text = this.answerEditor.value;
-            this.showFormatedAnswer(text);
+            let html = this.replaceNewLineByBreakTag(text);
+            this.showFormatedAnswer(html);
         }
     }
 
+    // private replaceUnnecessaryLineBreak(text:string):string{
+    //     let res = text.replace(this.orderListTags[0]+"\n\t"+"/g", this.orderListTags[0]);
+    //     //'\n'+this.orderListTags[1]
+    //     res = res.replace('\n'+this.orderListTags[1]+"/g", this.orderListTags[0]);
+    //     return res;
+    // }
+
+    private replaceNewLineByBreakTag(text: string):string {
+        let res = text.replace(/\n/g, "<br>");
+        return res;
+    }
+
+
     private addBoldListener() {
-        document.getElementById('boldBtn').addEventListener('click', ()=>{
-           let value = this.answerEditor.value;
-           let startIndex = this.answerEditor.selectionStart;
-           let endIndex = this.answerEditor.selectionEnd;
-           if(this.isBold(startIndex,endIndex)){
-                this.removeBold(startIndex,endIndex);
-                return;
-           }
-           this.addBold(startIndex,endIndex);
+        document.getElementById('boldBtn').addEventListener('click', () => {
+            this.toogleTag(this.boldTags);
         });
     }
 
-    private addBold(startIndex:number, endIndex:number){
-        let value = this.answerEditor.value;
-        let first_part = value.substring(0,startIndex);
-        let second_part = value.substring(endIndex,value.length);
-        let middle_part = value.substring(startIndex,endIndex);
-        if(middle_part == "") middle_part = "write here";
-        this.answerEditor.value = first_part +"**"+ middle_part+"**" + second_part;
-        
-        if(middle_part == "")this.selectText(startIndex+2, startIndex+12);
-    }
-
-    private removeBold(startIndex:number,endIndex:number){
-        let value = this.answerEditor.value;
-        let first_part = value.substring(0,startIndex-2);
-        let second_part = value.substring(endIndex+2,value.length);
-        let middle_part = value.substring(startIndex,endIndex);
-        this.answerEditor.value = first_part + middle_part + second_part;
-    }
-
-    private isBold(startIndex:number,endIndex:number):boolean{
-        let value = this.answerEditor.value;
-        let prefix = value.substring(startIndex-2,startIndex);
-        let suffix = value.substring(endIndex,endIndex+2);
-        if(prefix == "**" && suffix == "**") return true;
-        return false;
-    }
-
     private addItalicListener() {
+        document.getElementById('italicBtn').addEventListener('click', () => {
+            this.toogleTag(this.italicTags);
+        });
+    }
 
+    private addLinkListener(){
+        document.getElementById('linkBtn').addEventListener('click', () => {
+            this.toogleTag(this.linkTags);
+        });
     }
 
     private addUnderLineListener() {
+        document.getElementById('underLineBtn').addEventListener('click', () => {
+            this.toogleTag(this.underLineTags);
+        });
+    }
+
+    private addTabListener() {
+        document.getElementById('tabBtn').addEventListener('click', () => {
+            this.insertTag(this.tabTag);
+        });
+    }
+
+    private addOrderListListener(){
+        document.getElementById('orderListBtn').addEventListener('click', () => {
+            let startIndex = this.answerEditor.selectionStart;
+            this.insertTag(this.orderListTags[0]+"\n\t");
+            this.insertTag(this.listItemTags[0]+"write here"+this.listItemTags[1]);
+            this.insertTag('\n'+this.orderListTags[1]);
+            let selectionStart = startIndex+this.orderListTags[0].length+2+this.listItemTags[0].length;
+
+            this.selectText(selectionStart, selectionStart+ "write here".length);
+        });
+    }
+
+    private addUnOrderListListener(){
+        document.getElementById('unorderListBtn').addEventListener('click', () => {
+            let startIndex = this.answerEditor.selectionStart;
+            this.insertTag(this.unorderListTags[0]+"\n\t");
+            this.insertTag(this.listItemTags[0]+"write here"+this.listItemTags[1]);
+            this.insertTag('\n'+this.unorderListTags[1]);
+            let selectionStart = startIndex+this.unorderListTags[0].length+2+this.listItemTags[0].length;
+
+            this.selectText(selectionStart, selectionStart+ "write here".length);
+        });
+    }
+
+    private insertTag(tag:string){
+        let startIndex = this.answerEditor.selectionStart;
+        let value = this.answerEditor.value;
+        let first_part = value.substring(0, startIndex);
+        let second_part = value.substring(startIndex, value.length);
+        this.answerEditor.value = first_part + tag + second_part;
+    }
+
+    private toogleTag(tags: string[]) {
+        let value = this.answerEditor.value;
+        let startIndex = this.answerEditor.selectionStart;
+        let endIndex = this.answerEditor.selectionEnd;
+        if (this.isTagExist(startIndex, endIndex, tags)) {
+            this.removeTag(startIndex, endIndex, tags);
+            return;
+        }
+        this.addTag(startIndex, endIndex, tags);
 
     }
 
-    private addLinkListener() {
+    private addTag(startIndex: number, endIndex: number, tags: string[]) {
+        let value = this.answerEditor.value;
+        let first_part = value.substring(0, startIndex);
+        let second_part = value.substring(endIndex, value.length);
+        let middle_part = value.substring(startIndex, endIndex);
+        if (middle_part == "") {
+            middle_part = "write here";
+        }
+        this.answerEditor.value = first_part + tags[0] + middle_part + tags[1] + second_part;
 
+        this.selectText(startIndex + tags[0].length, startIndex + middle_part.length + tags[0].length);
+
+    }
+
+    private removeTag(startIndex: number, endIndex: number, tags: string[]) {
+        let value = this.answerEditor.value;
+        let first_part = value.substring(0, startIndex - tags[0].length);
+        let second_part = value.substring(endIndex + tags[1].length, value.length);
+        let middle_part = value.substring(startIndex, endIndex);
+        if (middle_part == "write here") middle_part = "";
+        this.answerEditor.value = first_part + middle_part + second_part;
+        this.selectText(startIndex - tags[0].length, startIndex + middle_part.length - tags[0].length);
+
+    }
+
+    private isTagExist(startIndex: number, endIndex: number, tags: string[]): boolean {
+        let value = this.answerEditor.value;
+        let prefix = value.substring(startIndex - tags[0].length, startIndex);
+        let suffix = value.substring(endIndex, endIndex + tags[1].length);
+        if (prefix == tags[0] && suffix == tags[1]) return true;
+        return false;
     }
 
     private addNewLineListener() {
 
     }
 
-    private addOrderListListener() {
-
-    }
     private addUnorderListListener() {
 
     }
@@ -89,7 +171,8 @@ class Answer {
 
     private showFormatedAnswer(text: string) {
         let elem = document.getElementById('answer_display');
-        elem.innerHTML = this.getHtml(text);
+        //elem.innerHTML = this.getHtml(text);
+        elem.innerHTML = text;
     }
 
     private getHtml(text: string): string {
