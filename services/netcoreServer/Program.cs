e59f6ac48441dd1bf49c3c46d954316f46eed542
tulshi das <bsse0811@iit.du.ac.bd>
2019-08-18 20:27:03 +0600

    dot net core server with react template is created

--- services/netcoreServer/Program.cs
+++ services/netcoreServer/Program.cs
@@ -17,8 +17,9 @@ namespace services
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        }
     }
 }
