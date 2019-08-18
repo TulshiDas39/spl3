577b3d15189aa0f10f972e93687263239880a824
tulshi das <bsse0811@iit.du.ac.bd>
2019-08-19 02:10:11 +0600

    integreted typescript in react

--- services/netcoreServerWithReact/ClientApp/src/components/NavMenu.tsx
+++ services/netcoreServerWithReact/ClientApp/src/components/NavMenu.tsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+// @ts-ignore
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
-export class NavMenu extends Component {
+interface ILocalState {
+  collapsed: boolean
+}
+
+export class NavMenu extends Component<{}, ILocalState> {
   static displayName = NavMenu.name;
 
-  constructor (props) {
+  constructor (props: any) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
@@ -26,7 +31,7 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">netcoreServerWithReact</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">BlogExample.Web</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
