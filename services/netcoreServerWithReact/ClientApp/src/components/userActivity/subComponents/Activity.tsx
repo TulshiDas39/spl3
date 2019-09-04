5866a2e48cf83a094915577dd43de46c4bdf8bb9
tulshi das <bsse0811@iit.du.ac.bd>
2019-09-04 23:52:10 +0600

    unnecessary service removed

--- services/netcoreServerWithReact/ClientApp/src/components/userActivity/subComponents/Activity.tsx
+++ services/netcoreServerWithReact/ClientApp/src/components/userActivity/subComponents/Activity.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styles/activity.css";
 import { Badge, badge } from "./Badge";
 import { UserActivity } from "../UserActivity";
