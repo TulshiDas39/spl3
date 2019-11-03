ac7d5003d0532acfc45073613f402927306f715d
tulshi das <bsse0811@iit.du.ac.bd>
2019-11-04 02:39:57 +0600

    association generated with apriori

--- ForumApi/Services/QuestionRecommendation.cs
+++ ForumApi/Services/QuestionRecommendation.cs
@@ -11,7 +11,7 @@ namespace ForumApi.Services
         private UserService _userService;
         private ILogger _logger;
         private double _minConfidence = 0.5;
-        private double _minSupport = 0.5;
+        private double _minSupport = 0.4;
 
         private int _minSupportCount;
 
@@ -19,7 +19,6 @@ namespace ForumApi.Services
         {
             _userService = userService;
             _logger = logger;
-            _logger.LogDebug("initialising logger for question recommendation");
         }
 
         public List<Question> Recommend(string userId, int iteration)
@@ -46,19 +45,29 @@ namespace ForumApi.Services
         {
             // int iteration = 1;
             _minSupportCount = (int)(_minSupport * tags.Count);
-            _logger.LogDebug("" + _minSupportCount);
+            Console.WriteLine("min support:" + _minSupportCount);
 
-            Dictionary<string, int> dic = GenerateCandidate(tags);
-            bool isFiltered = Filter(dic);
-            Dictionary<string, int> newCandidates = new Dictionary<string, int>();
+            Dictionary<string, int> newCandidates = GenerateCandidates(tags);
+            bool isFiltered = Filter(newCandidates);
+            Dictionary<string, int> oldCandidates = newCandidates;
+			Dictionary<string, int> associations = new Dictionary<string, int>();
 
-            while (true)
+            while (oldCandidates.Count() > 0)
             {
-                if (!isFiltered) break;
-                newCandidates = GenerateCandidate(dic);
+                newCandidates = GenerateCandidates(oldCandidates,tags);
+				Console.WriteLine("new candidates:");
                 isFiltered = Filter(newCandidates);
+                mergeCandidates(associations,newCandidates);
+                oldCandidates = newCandidates;
             }
 
+        }
+
+        private void mergeCandidates(Dictionary<string, int> dic, Dictionary<string, int> newCandidates)
+        {
+            foreach(var candidate in newCandidates){
+                dic.Add(candidate.Key, candidate.Value);
+            }
         }
 
         private bool Filter(Dictionary<string, int> dic)
@@ -82,37 +91,51 @@ namespace ForumApi.Services
 
         }
 
-        private Dictionary<string, int> GenerateCandidate(Dictionary<string, int> dic)
+        private Dictionary<string, int> GenerateCandidates(Dictionary<string, int> dic, List<string> tags)
         {
             Dictionary<string, int> newCandidates = new Dictionary<string, int>();
             int interSectCount = Utility.GetWords(dic.Keys.ElementAt(0)).Length - 1;
 
-            for(int i=0;i<dic.Keys.Count;i++)
+            for (int i = 0; i < dic.Keys.Count; i++)
             {
                 string key = dic.Keys.ElementAt(i);
-                string[] words= Utility.GetWords(dic.Keys.ElementAt(i));
+                string[] words = Utility.GetWords(dic.Keys.ElementAt(i));
 
-                for(int j=i+1;j<dic.Keys.Count;j++){
-                    string key2 = dic.Keys.ElementAt(j);
-                    string[] words2 = Utility.GetWords(key2);
-                    int intersection = words.Intersect(words2).Count();
-                    string newStr = Utility.ArrayToString(words.Union(words2));
-                    if(interSectCount == intersection) newCandidates.Add(newStr,0);
+                for (int j = i + 1; j < dic.Keys.Count; j++)
+                {
+                    CreateCandidate(dic, newCandidates, interSectCount, words, j, tags);
                 }
-                //Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
             }
-
-            MeasureSupport(newCandidates);
 
             return newCandidates;
         }
 
-        private void MeasureSupport(Dictionary<string, int> newCandidates)
+        private void CreateCandidate(Dictionary<string, int> dic, Dictionary<string, int> newCandidates, int interSectCount, string[] tags, int index, List<string> allTags)
         {
-            throw new NotImplementedException();
+            string key = dic.Keys.ElementAt(index);
+            string[] tags2 = Utility.GetWords(key);
+            int intersection = tags.Intersect(tags2).Count();
+            if (interSectCount == intersection)
+            {
+				IEnumerable<string> newCandidate = tags.Union(tags2);
+				string newCandidateStr = Utility.ArrayToString(newCandidate);
+                int support = MeasureSupport(newCandidate, allTags);
+                newCandidates.Add(newCandidateStr, support);
+            }
         }
 
-        private Dictionary<string, int> GenerateCandidate(List<string> tags)
+        private int MeasureSupport(IEnumerable<string> words2, List<string> tags)
+        {
+            int support = 0;
+            string[] words;
+            tags.ForEach(item=>{
+                words = Utility.GetWords(item);
+                if(words.Intersect(words2).Count() == words2.Count()) support++;
+            });
+
+            return support;
+        }
+        private Dictionary<string, int> GenerateCandidates(List<string> tags)
         {
             Dictionary<string, int> dic = new Dictionary<string, int>();
 
