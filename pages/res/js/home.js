69764865c182c132cee2664ea3a67da1eef1255f
tulshi das <bsse0811@iit.du.ac.bd>
2019-12-05 00:06:52 +0600

    static pages removed

--- pages/res/js/home.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-class Home {
-}
-//# sourceMappingURL=home.js.map
--- pages/res/js/test.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('btn').addEventListener('click', ()=>{
-    console.log('clicked');
-    let elem = document.getElementById('test');
-    console.log(elem.style.display);
-    if(elem.style.display == "none") elem.style.display = "block";
-    else if(elem.style.display == "block") elem.style.display = "none";
-    
-})
